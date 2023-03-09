929ac70cd5bc5717864e7d172382d4958bcde592
Sriram M <sriram.m@publicissapient.com>
2023-03-09 18:49:35 +0530

    add static fix #8

--- website/settings.py
+++ website/settings.py
@@ -34,8 +34,8 @@ STATIC_URL = 'static/'
 # else: 
 #     STATIC_ROOT = BASE_DIR / 'static'
 
-# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
