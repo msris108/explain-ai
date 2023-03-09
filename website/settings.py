13ff1b10b690cafab4ad069a07cba24d7a88a7d3
Sriram M <sriram.m@publicissapient.com>
2023-03-09 12:56:29 +0530

    add globalstaticfiles

--- website/settings.py
+++ website/settings.py
@@ -23,13 +23,14 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = False
 ALLOWED_HOSTS = ["*"]
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
+STATIC_URL = 'static/'
 
-if DEBUG:
-  STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-else:
-  STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR / 'productionfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'globalstaticfiles'
+] 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
@@ -96,12 +97,6 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-# }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -126,8 +121,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = "static/"
-
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = '/home'
@@ -143,10 +136,6 @@ REST_FRAMEWORK = {
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
-
-# CORS_ALLOWED_ORIGINS = [
-#     'https://localhost',
-# ]
 
 JAZZMIN_SETTINGS = {
     "site_title": "ExplainAI Admin",
